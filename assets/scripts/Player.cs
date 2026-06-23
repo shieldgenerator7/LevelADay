f484dcb782d6f50153d066cbc08f836d9b9332c8
shieldgenerator7 <shieldgenerator7@yahoo.com>
2026-06-22 22:18:54 -0400

    Switch animations*
    *from tutorial:
    https://youtu.be/oED12Mo2018

--- assets/scripts/Player.cs
+++ assets/scripts/Player.cs
@@ -4,17 +4,30 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
+	
+	[Export]
+	public AnimatedSprite2D animatedSprite2D {get; set;}
+	
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -850.0f;
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
+		
+		// Add animation
+		if (velocity.X > 1 || velocity.X < -1){
+			animatedSprite2D.Animation = "walk";
+		}
+		else{
+			animatedSprite2D.Animation = "idle";
+		}
 
 		// Add the gravity.
 		if (!IsOnFloor())
 		{
 			velocity += GetGravity() * (float)delta;
+			animatedSprite2D.Animation = "jump";
 		}
 
 		// Handle Jump.
