ca78b8d17112124c30c3dff11da0ec2ca0b850ba
shieldgenerator7 <shieldgenerator7@yahoo.com>
2026-06-23 04:09:45 -0400

    Read level data from a bitmap

--- assets/scripts/LevelTest.cs
+++ assets/scripts/LevelTest.cs
@@ -1,23 +1,41 @@
 using Godot;
 using System;
+using System.IO;
+//using System.Drawing;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class LevelTest : Node2D
 {
 	[Export]
+	public string filename;
+
+	[Export]
 	public TileMapLayer tileMap;
+
+
+	public Dictionary<string, int> pixelKeyMap = new Dictionary<string, int>
+	{
+		{"0_0_0", 0 },//black = empty space
+		{"255_255_255", 1 },//white = block
+	};
+
+	private int levelWidth = 0;
+	private int levelHeight = 0;
 
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 
-		int[,] mapData = getMapData();
+		int[,] mapData = readBitMapData(filename);
 		for (int x = 0; x < mapData.GetLength(0); x++)
 		{
 			for (int y= 0; y < mapData.GetLength(1); y++)
 			{
 				//x and y are switched on purpose here, idk why it has to be this way, but it works
-				int tileId = (mapData[y,x] == 1) ? 0 : -1;
+				int tileId = (mapData[x,y] == 1) ? 0 : -1;
 				tileMap.SetCell(new Vector2I(x, y), tileId, new Vector2I(0,0));
 			}
 		}
@@ -30,7 +48,36 @@ public partial class LevelTest : Node2D
 
 	private int[,] getMapData()
 	{
-		int[,] mapData = new int[3,3] { { 1, 1, 1}, { 1, 0, 0}, { 0, 1, 0} };
+		int[,] mapData = new int[3, 3] { { 1, 1, 1 }, { 1, 0, 0 }, { 0, 1, 0 } };
+		return mapData;
+	}
+	
+	private int[,] readBitMapData(string filename)
+	{
+		//TODO: check to make sure file is bitmap
+		//Bitmap bmp = new Bitmap();
+		int[,] mapData = new int[100, 100];
+		Image bmp = Image.LoadFromFile(filename);
+
+		
+
+		//2026-06-22: copied from https://learn.microsoft.com/en-us/dotnet/api/system.drawing.bitmap?view=net-11.0-pp
+		// Loop through the images pixels to reset color.
+		levelWidth = bmp.GetWidth();
+		levelHeight = bmp.GetHeight();
+		for (int x = 0; x < levelWidth; x++)
+		{
+			for (int y = 0; y < levelHeight; y++)
+			{
+				Color pixelColor = bmp.GetPixel(x, y);
+				string pixelKey = String.Concat((int)pixelColor.R*255, "_", (int)pixelColor.G*255, "_", (int)pixelColor.B*255);
+				GD.Print(pixelKey);
+				mapData[x, y] = pixelKeyMap[pixelKey];
+				//List<int> a = new List<int>();
+				//a.Any(x=>x>0);
+			}
+		}
+
 		return mapData;
 	}
 }
